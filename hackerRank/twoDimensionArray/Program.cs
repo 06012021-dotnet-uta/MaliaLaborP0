ad2744e0aeef674043b06ddcd9a0ea743a0a0b4c
Malia-Labor <malia.labor@revature.net>
2021-06-04 09:27:09 -0700

    adds twoDimensionalArray hacker rank challenge

---
+++ hackerRank/twoDimensionArray/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace twoDimensionArray
+{
+    class Result
+{
+
+    /*
+     * Complete the 'hourglassSum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int hourglassSum(List<List<int>> arr)
+    {
+        int highest = 0;
+        for(int x = 0; x < arr.Count - 2; x++)
+        {
+            for(int y = 0; y <arr[x].Count - 2; y++)
+            {
+                int sum = arr[x][y] + arr[x][y+1] + arr[x][y+2] + arr[x+1][y+1] + arr[x+2][y] + arr[x+2][y+1] + arr[x+2][y+2];
+                if(sum>highest)
+                {
+                    highest = sum;
+                }
+            }
+        }
+        return highest;
+    }
+
+}
+    class Solution
+{
+    public static void Main(string[] args)
+    {
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < 6; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.hourglassSum(arr);
+
+        Console.WriteLine(result);
+    }
+}
+
+}
