b88589671a83fadadfc273a3656b2689ed24b7fd
Malia-Labor <malia.labor@revature.net>
2021-06-07 14:08:56 -0700

    adds camelCase hacker rank challenge

---
+++ hackerRank/CamelCase/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace CamelCase
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'camelcase' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts STRING s as parameter.
+         */
+
+        public static int camelcase(string s)
+        {
+            //variable to count words
+            int wordCount = 1;
+            //loop through chars
+            foreach(char c in s)
+            {
+                //if capital, then new word, wordcount++
+                if(char.IsUpper(c))
+                    wordCount++;
+            }
+            return wordCount;
+        }
+
+    }
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string s = Console.ReadLine();
+
+            int result = Result.camelcase(s);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
