b950576eee7fd37867d78d2d548c082b22f3958a
Malia-Labor <malia.labor@revature.net>
2021-06-20 13:03:42 -0700

    added view models and changes to controllers and views

--- Projects/Project1/Project1/Controllers/ShopController.cs
+++ Projects/Project1/Project1/Controllers/ShopController.cs
@@ -1,13 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Project1DbContext;
+using Project1.Models;
 
 namespace Project1.Controllers
 {
     public class ShopController : Controller
     {
+        private readonly ILogger<ShopController> _logger;
+        private readonly ProductHandler _productHandler;
+
+        public ShopController(ProductHandler productHandler, ILogger<ShopController> logger)
+        {
+            this._productHandler = productHandler;
+            this._logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -15,7 +28,21 @@ namespace Project1.Controllers
 
         public IActionResult Products()
         {
-            return View();
+            // pass single table into view
+            //List<Product> productsList = _productHandler.ProductList();
+            //return View(productsList);
+
+            // join tables together to pass into view
+            var tables = from p in _productHandler.ProductList()
+                         join i in _productHandler.PicturesList() on p.Id equals i.ProductId into table1
+                         from i in table1.DefaultIfEmpty()
+                         select new ProductViewModel
+                         {
+                             productVm = p,
+                             productPictureVm = i
+                         };
+            // need to add shop inventory to join
+            return View(tables);
         }
 
         public IActionResult Edit()
